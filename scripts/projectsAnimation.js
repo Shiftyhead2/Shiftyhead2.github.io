2fc5ef5e81aba440d11182204a59b71df7729bf7
Shiftyhead2 <shiftyhead2@gmail.com>
2021-01-21 17:52:23 +0100

    Made the website a bit better

--- scripts/projectsAnimation.js
+++ scripts/projectsAnimation.js
@@ -1,7 +1,6 @@
 const articleContainer = document.querySelector(".games-intro");
 const gamesTitle = document.querySelector(".games-title");
 const gamesText1 = document.querySelector(".games-text1");
-const gamesText2 = document.querySelector(".games-text2");
 const projectArticles = document.querySelectorAll('.article');
 
 const projectArticlesFadeIn = () => {
@@ -9,9 +8,8 @@ const projectArticlesFadeIn = () => {
   articleContainer.style.animation = `fadein 0.5s ease forwards`;
   gamesTitle.style.animation = `articleFade 1s ease forwards 0.05s`;
   gamesText1.style.animation = `navLinksFade 1s ease forwards 0.07s`;
-  gamesText2.style.animation = `articleFade 1s ease forwards 0.09s`;
 
-  projectArticles.forEach((article,index) => {
+  projectArticles.forEach((article,index) => { 
     article.style.animation = `articleFade 1s ease forwards ${index / 2.5}s`
   });
 }
